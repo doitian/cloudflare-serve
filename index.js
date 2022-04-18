5c729ee37d07415c9cab6270504b30a35cd3efa1
ian <me@iany.me>
2022-04-18 21:09:25 +0800

    initial version

---
+++ index.js
@@ -0,0 +1,51 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+function guessMime(base, defaultMime) {
+  const lowercaseBase = base.toLowerCase()
+
+  if (lowercaseBase.endsWith('.html')) {
+    return 'text/html'
+  }
+  if (lowercaseBase.endsWith('.jpg') || lowercaseBase.endsWith('.jpeg')) {
+    return 'image/jpeg'
+  }
+  if (lowercaseBase.endsWith('.png')) {
+    return 'image/png'
+  }
+  if (lowercaseBase.endsWith('.svg')) {
+    return 'image/svg+xml'
+  }
+
+  return defaultMime
+}
+
+/**
+ * Respond with hello worker text
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+  const url = new URL(request.url)
+
+  const baseOriginURL = url.pathname.split(/:\/\/?/, 2).join('://')
+
+  const originURL = new URL([baseOriginURL, url.search, url.hash].join(''))
+  console.log(originURL)
+
+  const originResponse = await fetch(originURL)
+  if (originResponse.ok) {
+    let headers = new Headers(originResponse.headers)
+    headers.set(
+      'content-type',
+      guessMime(baseOriginURL, headers.get('content-type')),
+    )
+    return new Respond(originResponse.body, {
+      status: originResponse.status,
+      statusText: originResponse.statusText,
+      headers: headers,
+    })
+  }
+
+  return originResponse
+}
